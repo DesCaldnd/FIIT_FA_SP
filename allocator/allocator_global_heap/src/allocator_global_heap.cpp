9a5cef4be405e38935ddfdfd296595296b4e9f53
CrazyCatGames <paveldomkin@gmail.com>
2025-04-07 23:26:51 +0300

    Added logger messages

--- allocator/allocator_global_heap/src/allocator_global_heap.cpp
+++ allocator/allocator_global_heap/src/allocator_global_heap.cpp
@@ -6,14 +6,14 @@
 
 [[nodiscard]] void *allocator_global_heap::do_allocate_sm(size_t size) {
 	void *ptr;
-
+	debug_with_guard("Allocation of size " + std::to_string(size) + " started");
 	try {
 		ptr = ::operator new(size);
 	} catch (std::bad_alloc &e) {
-		error_with_guard("Failed to allocate memory of size" + std::to_string(size));
+		error_with_guard("Failed to allocate memory of size " + std::to_string(size));
 		throw;
 	} catch (std::exception &e) {
-		error_with_guard("Unexpected error :" + std::string(e.what()));
+		error_with_guard("Unexpected error: " + std::string(e.what()));
 		throw;
 	}
 
@@ -29,8 +29,11 @@ void allocator_global_heap::do_deallocate_sm(void *at) {
 		return;
 	}
 
+	std::ostringstream oss;
+	oss << "0x" << std::hex << reinterpret_cast<std::uintptr_t>(at);
+	debug_with_guard("Deallocation of memory at 0x" + oss.str() + " started");
 	::operator delete(at);
-	debug_with_guard("Successfully deallocated memory at 0x" + (std::ostringstream{} << std::hex << reinterpret_cast<std::uintptr_t>(at)).str());
+	debug_with_guard("Successfully deallocated memory at 0x" + oss.str() + " finished");
 }
 
 inline logger *allocator_global_heap::get_logger() const {
@@ -42,21 +45,25 @@ inline std::string allocator_global_heap::get_typename() const {
 }
 
 allocator_global_heap::allocator_global_heap(logger *logger) : _logger(logger) {
-	debug_with_guard("Constructor of allocator_global_heap called");
+	trace_with_guard("Constructor of allocator_global_heap started");
+	trace_with_guard("Constructor of allocator_global_heap finished");
 }
 
 allocator_global_heap::~allocator_global_heap() {
-	debug_with_guard("Destructor of allocator_global_heap called");
+	trace_with_guard("Destructor of allocator_global_heap started");
+	trace_with_guard("Destructor of allocator_global_heap finished");
 }
 
 allocator_global_heap::allocator_global_heap(const allocator_global_heap &other) : _logger(other._logger) {
-	debug_with_guard("Copy constructor of allocator_global_heap called");
+	trace_with_guard("Copy constructor of allocator_global_heap started");
+	trace_with_guard("Copy constructor of allocator_global_heap finished");
 }
 
 allocator_global_heap &allocator_global_heap::operator=(const allocator_global_heap &other) {
 	if (this != &other) {
-		debug_with_guard("Assignment constructor of allocator_global_heap called");
+		trace_with_guard("Assignment constructor of allocator_global_heap started");
 		_logger = other._logger;
+		trace_with_guard("Assignment constructor of allocator_global_heap finished");
 	}
 
 	return *this;
@@ -67,13 +74,15 @@ bool allocator_global_heap::do_is_equal(const std::pmr::memory_resource &other) 
 }
 
 allocator_global_heap::allocator_global_heap(allocator_global_heap &&other) noexcept : _logger(other._logger) {
-	debug_with_guard("Move constructor of allocator_global_heap called");
+	trace_with_guard("Move constructor of allocator_global_heap started");
+	trace_with_guard("Move constructor of allocator_global_heap finished");
 }
 
 allocator_global_heap &allocator_global_heap::operator=(allocator_global_heap &&other) noexcept {
 	if (this != &other) {
-		debug_with_guard("Move assignment constructor of allocator_global_heap called");
+		trace_with_guard("Move assignment constructor of allocator_global_heap started");
 		_logger = other._logger;
+		trace_with_guard("Move assignment constructor of allocator_global_heap finished");
 	}
 
 	return *this;
